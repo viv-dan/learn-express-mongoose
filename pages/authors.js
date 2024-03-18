682248f635d9875156eb3a72563bda7747841564
viv-dan <81843793+viv-dan@users.noreply.github.com>
2024-03-18 16:10:12 -0400

    class activity 031824

--- pages/authors.js
+++ pages/authors.js
@@ -1,14 +1,16 @@
-let Author = require('../models/author');
+let Author = require('../models/author')
 
 get_author_list = async () => {
-  let authors_list = [];
-  return authors_list.map(function(author) {
-    return author.name + " : " + author.lifespan;
-  });
-};
+  let authors_list = await Author.find()
+    .sort([['family_name', 'ascending']])
+    .exec()
+  return authors_list.map(function (author) {
+    return author.name + ' : ' + author.lifespan
+  })
+}
 
-exports.show_all_authors = function(res) {
+exports.show_all_authors = function (res) {
   get_author_list()
     .then((data) => res.send(data))
-    .catch((_) => res.send('No authors found'));
+    .catch((_) => res.send('No authors found'))
 }
--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,14 @@
-let BookInstance = require('../models/bookinstance');
+let BookInstance = require('../models/bookinstance')
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
+exports.show_all_books_status = function (res) {
+  BookInstance.find({ status: { $eq: 'Available' } })
+    .populate('book')
+    .exec()
+    .then((list) => {
+      res.send(
+        list.map(function (bookInstance) {
+          return bookInstance.book.title + ' : ' + bookInstance.book.status
+        })
+      )
+    })
 }
